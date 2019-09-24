3d80687b1d8c54392f38c8ae39538352e5a4b18f
Liubov Kaper <luba@Liubovs-MacBook-Pro.local>
2019-09-24 15:51:56 -0400

    added xcode project

---
+++ Swift Demo/Swift Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Demo
+//
+//  Created by Liubov Kaper  on 9/24/19.
+//  Copyright © 2019 Luba Kaper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
